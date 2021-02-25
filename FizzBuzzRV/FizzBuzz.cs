042d8de6b5718cfbd9cc76436de0f6f89784e91d
antoine2116 <51358897+antoine2116@users.noreply.github.com>
2021-02-25 16:55:27 +0100

    Prod - 1st Test FizzBuzz with 0

--- FizzBuzzRV/FizzBuzz.cs
+++ FizzBuzzRV/FizzBuzz.cs
@@ -8,5 +8,14 @@ namespace FizzBuzzRV
 {
     public static class FizzBuzz
     {
-    }
+		public static string Ask(int number)
+		{
+			if (number == 0)
+			{
+				throw new ArgumentException("0");
+			}
+			return "";
+		}
+
+	}
 }
